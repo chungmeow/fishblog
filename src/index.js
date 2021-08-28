f72730ca099338eda77077fe8bdfd2e407744b48
chungmeow <chungmeow@hotmail.com>
2021-08-27 21:45:07 -0500

    Fixed missing font and color def by adding missing files and an entry in html, added dart sass, copied sass files over

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/clean-blog.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 if (App) {
     ReactDOM.render(
-        //<React.StrictMode>
-           <App/>,
-        //</React.StrictMode>,
+        <React.StrictMode>
+           <App/>
+        </React.StrictMode>,
         document.getElementById('root')
     );
 }
