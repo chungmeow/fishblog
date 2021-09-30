f06cea72e34fa69aacdef017eebece753cacd53a
chungmeow <chungmeow@hotmail.com>
2021-09-29 20:06:47 -0500

    Removing unused file and renaming componenets to camelcase for consistancy

--- client/src/components/PostItem.js
+++ client/src/components/PostItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default function Postitem() {
+export default function PostItem() {
     return (
         <div className="post-preview">
             <Link to="/post">
